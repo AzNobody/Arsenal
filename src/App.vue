930d7952ec25aae6166f8b863a90739a7a100f07
Nicolas Ouellet-Duval <OueNi1932722@etu.cegepjonquiere.ca>
2023-11-26 17:25:28 -0500

    Ajout images, GameList (liste jeux), json jeux (100 jeux total)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
-<script setup>
-
-</script>
-
 <template>
   <div id="app">
     <router-view></router-view>
   </div>
+  <GameList />
 </template>
+
+<script>
+import GameList from "./views/GameList.vue";
+export default {
+  name: "App",
+  components: {
+    GameList: GameList,
+  },
+};
+</script>
 
 <style scoped>
 </style>
---
+++ src/views/GameList.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Jeux populaires</h1>
+
+  <div class="product-container">
+    <div class="product-box" v-for="game in mygames" :key="game.id">
+      <h3>{{ game.name }}</h3>
+      <img :src="'../../src/image/' + game.image" alt="Game image" style="width: 100%;"> <!--src="{{game.image}}-->
+    </div>
+  </div>
+</template>
+
+<script>
+import games from "@/json/games.json";
+
+export default {
+  name: "GameList",
+  data() {
+    return {
+      mygames: games
+    }
+  }
+};
+</script>
