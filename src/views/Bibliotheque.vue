8b2574207023bb6420b3c80170f3a3e21c1bb577
Chibou <evan.blachier@epitech.eu>
2023-12-07 18:35:53 +0100

    Page bibliothèque avec panel pour les jeux

--- src/views/Bibliotheque.vue
+++ src/views/Bibliotheque.vue
@@ -1,9 +1,13 @@
 <script setup>
-import TopBarComponent from "@/components/pages/TopBarComponent.vue";
+import TopBarComponent from "@/components/pages/TopBarComponent.vue"
+import BiblioComponent from "@/components/pages/BiblioComponent.vue"
 </script>
 
 <template>
-  <TopBarComponent></TopBarComponent>
+  <div id="BiblioPage">
+    <TopBarComponent></TopBarComponent>
+    <BiblioComponent></BiblioComponent>
+  </div>
 </template>
 
 <style scoped>
